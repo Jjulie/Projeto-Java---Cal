87b00e4b9592b49cdf35a1490a7e83e6e823d871
Jjulie <julianesilva.oficial@gmail.com>
2016-05-10 21:32:18 -0300

    novo teste

--- src/Calcul.java
+++ src/Calcul.java
@@ -7,7 +7,12 @@ public class Calcul {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Calculadora c = new Calculadora();
 		
+		System.out.println(c.Somar(6,2));
+		System.out.println(c.Dividir(10, 5));
+		System.out.println(c.Subtrair(2,6));
+		System.out.println(c.Multiplicar(12,6));
 	}
 
 }
