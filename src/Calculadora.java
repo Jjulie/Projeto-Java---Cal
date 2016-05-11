6c991342f55724e4fd969e571f38de61b70f269f
Jjulie <julianesilva.oficial@gmail.com>
2016-05-10 21:14:36 -0300

    Calculadora

---
+++ src/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * versao 1.0.0
+ */
+public class Calculadora {
+
+	public double Somar(double j, double x){
+	return j+x;
+	}
+	
+	public double Subtrair(double j, double x){
+	return j+x;
+	}
+	
+	public double Multiplicar(double j, double x){
+	return j+x;
+	}
+	
+	public double Dividir(double j, double x){
+	return j+x;
+	}
+}
