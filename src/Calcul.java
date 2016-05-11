a327802fec59d26585890c69aa66618f93045ee9
Jjulie <julianesilva.oficial@gmail.com>
2016-05-10 21:40:38 -0300

    Alteração

--- src/Calcul.java
+++ src/Calcul.java
@@ -9,10 +9,8 @@ public class Calcul {
 
 		Calculadora c = new Calculadora();
 		
-		System.out.println(c.Somar(6,2));
-		System.out.println(c.Dividir(10, 5));
-		System.out.println(c.Subtrair(2,6));
-		System.out.println(c.Multiplicar(12,6));
+		
 	}
 
+	
 }
